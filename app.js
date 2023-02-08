3f2b17ff0f681dc73e25448bc736975a658a70b7
Jinyang Liu <lovejoyyoung@gmail.com>
2023-02-08 16:13:44 -0500

    add comments

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const routes = {
     '/dashboard': { templateId: 'dashboard' },
 };
 
+// Cloning a template before attaching it to the DOM is a common practice in order to avoid modifying the original template.If the original template is modified, it will affect all instances of the template and can lead to unintended consequences and bugs.
+// By cloning the template, a new, separate copy of the template is created that can be modified without affecting the original template.This allows for multiple instances of the same template to be used and manipulated independently of each other.
+// If the cloning step is skipped, the original template would be directly modified every time it is used, which could result in unexpected behavior and bugs.The original template may not be reusable, as it would contain the changes made to the previous instance.
+// In summary, cloning the template before attaching it to the DOM allows for multiple instances of the same template to be used and modified independently, while avoiding unintended consequences and preserving the original template.
+
 function navigate(path) {
     window.history.pushState({}, path, path);
     updateRoute();
@@ -36,3 +41,6 @@ function onLinkClick(event) {
 window.onpopstate = () => updateRoute();
 updateRoute();
 
+// The history.pushState method is part of the HTML5 standard and implemented in all modern browsers.
+// If you're building a web app for older browsers, there's a trick you can use in place of this API: using a hash(#) before the path you can implement routing that works with regular anchor navigation and does not reload the page, 
+// as it's purpose was to create internal links within a page.
