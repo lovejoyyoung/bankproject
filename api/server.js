133c74a3b57e47a77c9eff5c5a0613fcc21ec4d1
Jinyang Liu <lovejoyyoung@gmail.com>
2023-02-08 19:47:54 -0500

    port modified

--- api/server.js
+++ api/server.js
@@ -5,7 +5,7 @@ const crypto = require('crypto');
 const pkg = require('./package.json');
 
 // App constants
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 5000;
 const apiPrefix = '/api';
 
 // Store data in-memory, not suited for production use!
